5b3b32883a853c5a31b3e14ff6c60bccefad6d48
tomasmota <tomasrebelomota@gmail.com>
2023-04-07 21:31:10 +0200

    first attempt at fetching data (fail)
    Signed-off-by: tomasmota <tomasrebelomota@gmail.com>

--- src/components/PulsarPage/PulsarPage.tsx
+++ src/components/PulsarPage/PulsarPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Box,
   Typography,
@@ -9,6 +9,32 @@ import {
   TableRow,
   TableCell,
 } from '@material-ui/core';
+
+type PublisherStats = {
+  msgRateIn: number;
+};
+
+type TopicStats = {
+  publishers: PublisherStats[];
+};
+
+async function getPulsarTopicMessageCount(topic: string): Promise<number> {
+  const pulsarAdminApiBaseUrl = 'http://localhost:8080/';
+  const url = `${pulsarAdminApiBaseUrl}admin/v2/persistent/public/default/${topic}/stats`; // TODO: Parameterize tenant and namespace
+
+  const response = await fetch(url);
+
+  if (response.ok) {
+    const data: TopicStats = await response.json();
+    const messageCount = data.publishers.reduce(
+      (count, publisher) => count + publisher.msgRateIn,
+      0,
+    );
+    return messageCount;
+  } else {
+    throw new Error('Failed to fetch Pulsar topic stats');
+  }
+}
 
 const fakeData = {
   topics: [
@@ -38,6 +64,21 @@ const fakeData = {
 };
 
 export const PulsarPage = () => {
+  const [messageCount, setMessageCount] = useState<number | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const count = await getPulsarTopicMessageCount('bla');
+        setMessageCount(count);
+      } catch (error) {
+        console.error('Error fetching message count:', error);
+        setMessageCount(null);
+      }
+    };
+
+    fetchData();
+  }, []);
   const pulsarData = fakeData;
 
   return (
@@ -81,7 +122,18 @@ export const PulsarPage = () => {
       <Box mb={2}>
         <Typography variant="h5">Storage</Typography>
         <Typography>Used storage: {pulsarData.storage.used} GB</Typography>
-        <Typography>Available storage: {pulsarData.storage.available} GB</Typography>
+        <Typography>
+          Available storage: {pulsarData.storage.available} GB
+        </Typography>
+      </Box>
+
+      <Box mb={2}>
+        <Typography variant="h6">Number of Messages:</Typography>
+        {messageCount !== null ? (
+          <Typography variant="body1">{messageCount}</Typography>
+        ) : (
+          <Typography variant="body1">Error fetching message count</Typography>
+        )}
       </Box>
     </Box>
   );
